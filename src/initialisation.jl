a3caf341f7a592765d2df2adb2e5c30c0a4e52f1
Francesca Capel <capel.francesca@gmail.com>
2021-04-26 16:28:21 +0200

    Add library location as input to qcinit() and test

--- src/initialisation.jl
+++ src/initialisation.jl
@@ -4,14 +4,15 @@
 Initialise QCDNUM - should be called before anything else. 
 
 # Arguments
+- `qcdnum_lib::String`: location of the QCDNUM library
 - `lun::Integer`: the output logical unit number. When set to 6, 
 the QCDNUM messages appear on the standard output. When set to -6, 
 the QCDNUM banner printout is suppressed.
 - `filename::String`: the output filename to store log.
 """
-function qcinit(lun::Integer, output_file::String)
+function qcinit(qcdnum_lib::String, lun::Integer, output_file::String)
 
-    qcdnum = Libdl.dlopen("/usr/local/lib/libQCDNUM.dylib")
+    qcdnum = Libdl.dlopen(qcdnum_lib, RTLD_NOW | RTLD_GLOBAL)
     
     lun = Ref{Int32}(lun)
  
