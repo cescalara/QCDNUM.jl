75d73b7c46ed0e3adadf2dcfb17df6b99292d36a
Francesca Capel <capel.francesca@gmail.com>
2021-11-26 20:47:32 +0100

    Parameters tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -85,6 +85,14 @@ end
     # Initialisation
     QCDNUM.qcinit(-6, "")
 
+    # Order
+    for iord in [1, 2, 3]
+        @test QCDNUM.setord(iord) == nothing
+    end
+
+    @test QCDNUM.setalf(0.364, 2.0) == nothing
+
+    @test QCDNUM.setabr(1.0, 0.0) == nothing 
     
 end
 
