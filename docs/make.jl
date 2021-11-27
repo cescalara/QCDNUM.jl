649f3d3b7c7af9512eae360eedb6d877a2899ad7
Francesca Capel <capel.francesca@gmail.com>
2021-11-27 18:11:44 +0100

    Add SPLINT docs

--- docs/make.jl
+++ docs/make.jl
@@ -10,16 +10,19 @@ gen_content_dir = joinpath(@__DIR__, "src")
 example_src = joinpath(@__DIR__, "..", "examples", "example.jl")
 testsgns_src = joinpath(@__DIR__, "..", "examples", "testsgns.jl")
 timing_src = joinpath(@__DIR__, "..", "examples", "timing.jl")
-    
+splint_src = joinpath(@__DIR__, "..", "examples", "splint.jl")
+     
 Literate.markdown(example_src, gen_content_dir, name="example")
 Literate.markdown(testsgns_src, gen_content_dir, name="testsgns")
 Literate.markdown(timing_src, gen_content_dir, name="timing")
+Literate.markdown(splint_src, gen_content_dir, name="splint")
 
 About = "Introduction" => "index.md"
 
 Installation = "Installation" => "installation.md"
 
-Examples = "Examples" => ["examples.md", "example.md", "testsgns.md", "timing.md"]
+Examples = "Examples" => ["examples.md", "example.md", "testsgns.md",
+                          "timing.md", "splint.md"]
 
 Functions = "Available functions" => "functions.md"
 
