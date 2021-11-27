5cc7fb7aecefc726eda0934dbce7a3a378923a0b
Francesca Capel <capel.francesca@gmail.com>
2021-11-27 18:04:21 +0100

    Switch over example.md, testsgns.md and timing.md

--- docs/make.jl
+++ docs/make.jl
@@ -6,15 +6,20 @@ using Literate
 
 # Generate examples notebooks
 gen_content_dir = joinpath(@__DIR__, "src")
-example_src = joinpath(@__DIR__, "..", "examples", "example.jl")
 
+example_src = joinpath(@__DIR__, "..", "examples", "example.jl")
+testsgns_src = joinpath(@__DIR__, "..", "examples", "testsgns.jl")
+timing_src = joinpath(@__DIR__, "..", "examples", "timing.jl")
+    
 Literate.markdown(example_src, gen_content_dir, name="example")
+Literate.markdown(testsgns_src, gen_content_dir, name="testsgns")
+Literate.markdown(timing_src, gen_content_dir, name="timing")
 
 About = "Introduction" => "index.md"
 
 Installation = "Installation" => "installation.md"
 
-Examples = "Examples" => "examples.md"
+Examples = "Examples" => ["examples.md", "example.md", "testsgns.md", "timing.md"]
 
 Functions = "Available functions" => "functions.md"
 
--- examples/timing.jl
+++ examples/timing.jl
@@ -1,35 +1,35 @@
-# ## QCDNUM example program
-#
-# Based on `example.f` detailed [here](https://www.nikhef.nl/~h24/qcdnum-files/testjobs1701/example.f).
+# ## Timing evolution and structure function evaluation
+
+# Based on the `timing.f` test job.
 
 using QCDNUM
 using Printf
 
-# Let's define some useful inputs
+# Again, we start by defining the necessary inputs...
 
-xmin = Float64.([1.0e-4])
-iwt = Int32.([1])
-ng = 1
+xmin = Float64.([1e-5, 0.2, 0.4, 0.6, 0.75])
+iwt = Int32.([1, 2, 4, 8, 16])
+ngx = 5
 nxin = 100
 iosp = 3
-nx = 10
-
-qq = Float64.([2e0, 1e4])
+nx = 0
+qlim = Float64.([2e0, 1e4])
 wt = Float64.([1e0, 1e0])
-nq = 1
-nqin = 60
 ngq = 2
+nqin = 50
+nq = 1
 itype = 1
-
+filename = "weights/unpolarised.wgt"
+iord = 3
 as0 = 0.364
 r20 = 2.0
-
 q2c = 3.0
 q2b = 25.0
 q0 = 2.0
-iqt = 999
+nfin = 0
+iqt = 999;
 
-def = Float64.([0., 0., 0., 0., 0.,-1., 0., 1., 0., 0., 0., 0., 0.,     
+def = Float64.([0., 0., 0., 0., 0.,-1., 0., 1., 0., 0., 0., 0., 0.,      
       0., 0., 0., 0.,-1., 0., 0., 0., 1., 0., 0., 0., 0.,      
       0., 0., 0.,-1., 0., 0., 0., 0., 0., 1., 0., 0., 0.,      
       0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0.,      
@@ -41,15 +41,6 @@ def = Float64.([0., 0., 0., 0., 0.,-1., 0., 1., 0., 0., 0., 0., 0.,
       0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,      
       0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,      
       0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]);
-
-nfin = 0
-x = 1.0e-3
-q = 1.0e3
-pdf = Array{Float64}(undef, 13)
-qmz2 = 8315.25;
-
-# Now, we can define the input PDF function for the PDF we would like to evolve. We also then have to use the
-# Julia `@cfunction` macro so that we can pass it to QCDNUM through the wrapper.
 
 function func(ipdf, x)::Float64
     i = ipdf[]
@@ -105,25 +96,51 @@ end
 
 func_c = @cfunction(func, Float64, (Ref{Int32}, Ref{Float64}))
 
-# Next, let's initialise QCDNUM and run QCDNUM to evolve the PDF.
+proton = Float64.([4.,1.,4.,1.,4.,1.,0.,1.,4.,1.,4.,1.,4.])/9.0;
+xx = Array{Float64}(undef, 1000)
+q2 = Array{Float64}(undef, 1000)
+
+roots = 300
+shera = roots*roots
+xlog1 = log(xmin[1])
+xlog2 = log(0.99)
+qlog1 = log(qlim[1])
+qlog2 = log(qlim[ngq])
+ntot = 1
+while (ntot < 1001)
+    rval = rand()
+    xlog = xlog1 + rval*(xlog2-xlog1)
+    xxxx = exp(xlog)
+    rval = rand()
+    qlog = qlog1 + rval*(qlog2-qlog1)
+    qqqq = exp(qlog)
+    if (qqqq <= xxxx*shera)
+        xx[ntot] = xxxx
+        q2[ntot] = qqqq
+        global ntot += 1
+    end
+end
+
+# Now, we set up QCDNUM and time many evolution and
+# structure function evaluations.
 
 QCDNUM.qcinit(-6, " ")
-nx = QCDNUM.gxmake(xmin, iwt, ng, nxin, iosp)
-nq = QCDNUM.gqmake(qq, wt, ngq, nqin)
-nw = QCDNUM.fillwt(itype)
-QCDNUM.setord(3)
+nx = QCDNUM.gxmake(xmin, iwt, ngx, nxin, iosp)
+nq = QCDNUM.gqmake(qlim, wt, ngq, nqin)
+nw = QCDNUM.fillwt(1)
+nw = QCDNUM.zmfillw()
+QCDNUM.setord(iord)
 QCDNUM.setalf(as0, r20)
 iqc = QCDNUM.iqfrmq(q2c)
 iqb = QCDNUM.iqfrmq(q2b)
-iqt = QCDNUM.iqfrmq(1e11)
-QCDNUM.setcbt(0, iqc, iqb, 0)
+QCDNUM.setcbt(nfin, iqc, iqb, iqt)
 iq0 = QCDNUM.iqfrmq(q0)
-eps = QCDNUM.evolfg(itype, func_c, def, iq0)
-pdf = QCDNUM.allfxq(itype, x, q, 0, 1)
-asmz, a, b = QCDNUM.asfunc(qmz2)
-csea = 2*pdf[3];
 
-# Now, we can briefly check the results
-
-@printf("x, q, CharmSea = %0.4e, %0.4e, %0.4e\n", x, q, csea)
-@printf("as(mz2) = %0.4e", asmz)
+@printf(" Wait: 1000 evols and 2.10^6 stfs will take ... ")
+@time begin
+    for iter in 1:1000
+        eps = QCDNUM.evolfg(itype, func_c, def, iq0)
+        ff = QCDNUM.zmstfun(1, proton, xx, q2, 1000, 1)
+        ff = QCDNUM.zmstfun(2, proton, xx, q2, 1000, 1)
+    end
+end
